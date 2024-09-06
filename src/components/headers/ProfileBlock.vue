77d1173f7275fb7225d5953334c269173c417cda
StasStepaniuk <stas.stepaniuk@precoro.com>
2024-09-06 23:18:46 +0300

    add profile block for header

---
+++ src/components/headers/ProfileBlock.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="profile-block">
+    <span>{{ 'nickname' }}</span>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="scss">
+.profile-block {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  color: var(--white);
+}
+</style>
