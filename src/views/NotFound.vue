269d84872282f07800d4452c3e12e177cf3882cd
StasStepaniuk <stas.stepaniuk@precoro.com>
2024-09-07 13:04:46 +0300

    refactor 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,44 +1,37 @@
 <template>
   <div class="bk__not_found">
-    <h1 class="error-code">404</h1>
-    <h2 class="error-text">
-      Page with path <span v-if="route" class="current-route">{{ route }}</span> not found
-    </h2>
-    <h2 class="error-text">Back to <RouterLink to="/">Home</RouterLink></h2>
+    <h1 class="error-title">Page not found</h1>
+    <h3 class="error-text">The page you are looking for is gone or never existed.</h3>
+    <BkButton href="/" content="Back to Homepage" type="outline" />
   </div>
 </template>
 
 <script setup>
-import { computed } from 'vue';
-import { useRouter } from 'vue-router';
-
-const router = useRouter();
-
-const route = computed(() => router.currentRoute.value.path);
+import BkButton from '@/components/base/BkButton.vue';
 </script>
 
 <style scoped lang="scss">
 .bk__not_found {
   height: 100%;
+  padding-left: 15%;
+  padding-top: 15%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 8px;
+  gap: 16px;
 
-  .error-code {
-    color: var(--primary-900);
-    font-size: 192px;
+  .error-title {
+    max-width: 45%;
+    background: var(--text-gradient);
+    -webkit-background-clip: text;
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-size: 64px;
   }
-  .error-text {
-    color: var(--accent-500);
-    font-size: 24px;
 
-    .current-route,
-    a {
-      color: var(--accent-700);
-      text-decoration: underline;
-    }
+  .error-text {
+    font-weight: 500;
+    font-size: 20px;
+    color: var(--neutral-500);
   }
 }
 </style>
