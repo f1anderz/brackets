3712e0e9db4b71fc89b729eeaa261163581f7fbc
StasStepaniuk <stas.stepaniuk@precoro.com>
2024-12-24 17:38:37 +0200

    refactor button and add primary variant

--- src/components/base/BkButton.vue
+++ src/components/base/BkButton.vue
@@ -1,28 +1,35 @@
 <template>
-  <button :class="classes">
-    <RouterLink v-if="href" :to="href" class="link">{{ t(contentKey) }}</RouterLink>
-    <slot v-else />
-  </button>
+  <component :is="href ? 'RouterLink' : 'button'" :to="href ? href : undefined" :class="classes">
+    <slot>{{ contentKey ? t(contentKey) : content }}</slot>
+  </component>
 </template>
 
 <script setup>
 import { computed } from 'vue';
 import { useI18n } from 'vue-i18n';
 
-const { t } = useI18n();
-
 const props = defineProps({
-  contentKey: { type: String },
-  type: { type: String, default: 'filled' },
+  content: {
+    type: String,
+    required: false,
+  },
+  contentKey: {
+    type: String,
+    required: false,
+  },
+  type: {
+    type: String,
+    default: 'primary',
+  },
   href: {
     type: String,
     default: undefined,
   },
 });
 
-const classes = computed(() => {
-  return `bk-button ${props.type}`;
-});
+const { t } = useI18n();
+
+const classes = computed(() => `bk-button ${props.type} ${props.href ? 'link' : ''}`);
 </script>
 
 <style scoped lang="scss">
@@ -42,9 +49,16 @@ const classes = computed(() => {
   color: var(--button-color);
   border: 1px solid var(--button-border);
   border-radius: 24px;
+  cursor: pointer;
 
   .link {
     color: var(--button-color);
+  }
+
+  &.primary {
+    --button-color: var(--white);
+    --button-background: var(--primary-800);
+    --button-border: var(--primary-900);
   }
 
   &.outline {
