59ecd66162f03d5515fbf40df94ccb2b37ac4dc2
StasStepaniuk <stas.stepaniuk@precoro.com>
2024-12-25 15:30:52 +0200

    fix naming

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import './assets/scss/main.scss';
-import router from './router/adindex.ts';
+import router from './router';
 import { createI18nInstance } from '@/utils/initI18n.ts';
 
 const app = createApp(App);
